f9dba4a4fb7d485f558d68a40ba41b583cf77e6d
Florencia Lema <flor.lema222@gmail.com>
2022-05-10 17:46:12 -0300

    not finished

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,26 +1,3 @@
 class Booking < ApplicationRecord
   belongs_to :car
-
-  def create
-    @booking = Booking.new(booking_params)
-    @car = Car.find(params[:car_id])
-    @booking.car = @car
-    if @booking.save
-      redirect_to car_path(@car)
-    else
-      render 'cars/show'
-    end
-  end
-
-  def destroy
-    @booking = Booking.find(params[:id])
-    @booking.destroy
-    redirect_to car_path(@booking.car)
-  end
-
-  private
-
-  def booking_params
-    params.require(:booking).permit(:fecha_inicio, :fecha_fin)
-  end
 end
