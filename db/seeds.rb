af21eceeacb9e9bf67cc7ac64b3ff702997d7186
Florencia Lema <flor.lema222@gmail.com>
2022-05-12 14:03:14 -0300

    fixed seeds and cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: "felipe.lisperguer@gmail.com", password: "123456", password_confirmation: "123456")
-Car.create(modelo: "chevrolet", anio: "2014", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user_id: 1)
+require "open-uri"
 
-User.create(email: "flo.lema@gmail.com", password: "123456", password_confirmation: "123456")
-Booking.create(fecha_inicio: "20/05/2022", fecha_fin: "20/08/2022", car_id: 1, user_id: 1)
+Car.destroy_all
 
+User.destroy_all
+
+Booking.destroy_all
+
+feli = User.create(email: "felipe.lisperguer@gmail.com", password: "123456", password_confirmation: "123456")
+flor = User.create(email: "flo.lema@gmail.com", password: "123456", password_confirmation: "123456")
+
+car_uno = Car.create!(modelo: "chevrolet", anio: "2014", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: flor)
+
+url = "https://www.pngall.com/wp-content/uploads/11/Tuning-Car-PNG-Photo.png"
+car_uno.photo.attach(io: URI.open(url), filename: "carsample.png", content_type: "image/jpeg")
+car_uno.save
+Booking.create(fecha_inicio: "20/05/2022", fecha_fin: "20/08/2022", car: car_uno, user: feli)
