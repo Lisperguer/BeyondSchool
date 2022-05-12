e1363585c4ccde084a309ffd1b931ba01f6ef0f6
Florencia Lema <flor.lema222@gmail.com>
2022-05-12 11:40:37 -0300

    dsasd

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cars do
-    resources :bookings, only: [:new]
+    resources :bookings, only: [:new, :create]
   end
-  resources :bookings, except: [:new]
+  resources :bookings, except: [:new, :create]
 end
