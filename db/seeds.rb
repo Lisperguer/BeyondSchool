c803c1df46b4349b809121c0260bd633f2649b41
Felipe Lisperguer <felipe.lisperguer.h@gmail.com>
2022-05-13 14:25:36 -0400

    Banner agregado

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,6 @@ feli = User.create(email: "felipe.lisperguer@gmail.com", password: "123456", pas
 flor = User.create(email: "flo.lema@gmail.com", password: "123456", password_confirmation: "123456")
 
 car_uno = Car.create!(modelo: "chevrolet", anio: "2014", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: flor)
-car_2 = Car.create!(modelo: "chevrolet", anio: "2014", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: feli)
-car_3 = Car.create!(modelo: "chevrolet", anio: "2016", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: flor)
-car_4 = Car.create!(modelo: "toyota", anio: "2018", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: feli)
-car_5 = Car.create!(modelo: "toyota", anio: "2014", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: flor)
-car_6 = Car.create!(modelo: "toyota", anio: "2020", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: feli)
-car_7 = Car.create!(modelo: "Volkswagen", anio: "2016", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: flor)
-car_8 = Car.create!(modelo: "Volkswagen", anio: "2018", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: feli)
-car_9 = Car.create!(modelo: "Renault", anio: "2020", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: flor)
-car_10 = Car.create!(modelo: "Renault", anio: "2018", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: feli)
-car_11 = Car.create!(modelo: "Ford", anio: "2018", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: flor)
-car_12 = Car.create!(modelo: "Ford", anio: "2018", precio: 2000, ubicacion: "Chile", detalles: "muy bonito", capacidad: 4, user: feli)
 
 
 url = "https://www.pngall.com/wp-content/uploads/11/Tuning-Car-PNG-Photo.png"
